56fdbefc7d807059461ca14572d0fb1b4fb93659
Irena Chen <irena1009@gmail.com>
2021-05-03 15:37:05 -0700

    build feed frame & get feeds api

--- public/main.js
+++ public/main.js
@@ -1,8 +1,8 @@
 document.querySelector("#share-post").addEventListener("click", toggleForm)
-console.log("main")
+//console.log("main")
 function toggleForm(){
     const shareFormStyle = document.querySelector('.share-form').style
-    console.log(shareFormStyle["max-height"])
+    //console.log(shareFormStyle["max-height"])
     if (shareFormStyle["max-height"] == "" || shareFormStyle["max-height"] == "0px"){
         shareFormStyle.opacity = 1
         shareFormStyle["max-height"] = "500px"
